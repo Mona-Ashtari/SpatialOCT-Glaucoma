0d895f6ccac3ed92254ee4b0f23817e45adb4659
Mona Ashtari-Majlan <89773228+Mona-Ashtari@users.noreply.github.com>
2024-12-12 20:43:40 +0100

    data_preparation.py

--- utils/data_preparation.py
+++ utils/data_preparation.py
@@ -1,5 +1,7 @@
 import os
-from sklearn.model_selection import KFold
+from sklearn.model_selection import StratifiedKFold, train_test_split
+import re
+import numpy as np
 
 
 def cross_validation_indices(data_path, num_folds):
@@ -13,36 +15,45 @@ def cross_validation_indices(data_path, num_folds):
     Returns:
     - A tuple of indices for training, validation, and testing splits for each class.
     """
+    img_list = os.listdir(data_path)
 
-    class0_index_split_train = []
-    class0_index_split_test = []
-    class0_index_split_val = []
+    cls_healthy = set()
+    cls_gl = set()
 
-    class1_index_split_train = []
-    class1_index_split_test = []
-    class1_index_split_val = []
+    for file in img_list:
+        if file.endswith(".npy"):
+            match = re.search(r'-(\d+)-', file)
+            img_id = match.group(1)
+            if 'Normal' in file:
+                cls_healthy.add(img_id)
+            elif 'POAG' in file:
+                cls_gl.add(img_id)
+
+    cls_healthy = list(cls_healthy)
+    cls_gl = list(cls_gl)
+
+    img_ids = np.array(cls_healthy + cls_gl)
+    labels = np.array([0] * len(cls_healthy) + [1] * len(cls_gl))  # 0 for healthy, 1 for glaucoma
 
     val_fraction = 0.1
 
-    kf = KFold(n_splits=num_folds, shuffle=True)
+    skf = StratifiedKFold(n_splits=num_folds, shuffle=True, random_state=42)
 
-    for train_index, test_index in kf.split(os.listdir(os.path.join(data_path, 'class0'))):
-        val_size = int(len(train_index) * val_fraction)
-        val_indices = np.random.choice(train_index, size=val_size, replace=False)
-        train_indices = [i for i in train_index if i not in val_indices]
+    index_split_train = {}
+    index_split_test = {}
+    index_split_val = {}
 
-        class0_index_split_train.append(train_indices)
-        class0_index_split_val.append(val_indices)
-        class0_index_split_test.append(test_index)
+    for fold, (train_idx, test_idx) in enumerate(skf.split(img_ids, labels)):
+        print(f'Preparing subset, fold {fold}')
+        train_img_ids, test_img_ids = img_ids[train_idx], img_ids[test_idx]
+        train_labels, test_labels = labels[train_idx], labels[test_idx]
 
-    for train_index, test_index in kf.split(os.listdir(os.path.join(data_path, 'class1'))):
-        val_size = int(len(train_index) * val_fraction)
-        val_indices = np.random.choice(train_index, size=val_size, replace=False)
-        train_indices = [i for i in train_index if i not in val_indices]
+        # Split the training set to get 10% validation set
+        train_img_ids, val_img_ids, train_labels, val_labels = train_test_split(
+            train_img_ids, train_labels, test_size=val_fraction, stratify=train_labels, random_state=42)
 
-        class1_index_split_train.append(train_indices)
-        class1_index_split_val.append(val_indices)
-        class1_index_split_test.append(test_index)
+        index_split_train[f'fold{fold}'] = [train_img_ids, train_labels]
+        index_split_test[f'fold{fold}'] = [test_img_ids, test_labels]
+        index_split_val[f'fold{fold}'] = [val_img_ids, val_labels]
 
-    return class0_index_split_train, class0_index_split_val, class0_index_split_test\
-        , class1_index_split_train, class1_index_split_val, class1_index_split_test
+    return index_split_train, index_split_test, index_split_val
